11385843d6515d9f14a90310e2139b0faf1195ca
junhey <junhey@qq.com>
2018-01-15 22:27:10 +0800

    update

---
+++ python-resource/filteringdata.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+from math import sqrt
+
+users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
+         "Bill":{"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0},
+         "Chan": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5, "Slightly Stoopid": 1.0},
+         "Dan": {"Blues Traveler": 3.0, "Broken Bells": 4.0, "Deadmau5": 4.5, "Phoenix": 3.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 2.0},
+         "Hailey": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0, "Vampire Weekend": 1.0},
+         "Jordyn":  {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 4.0},
+         "Sam": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0, "Slightly Stoopid": 4.0, "The Strokes": 5.0},
+         "Veronica": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5, "The Strokes": 3.0}
+        }
+
+def manhattan(rating1,rating2):
+    distance = 0;
+    commonRatings = False;
+    for key in rating1: 
+        if key in rating2:
+            distance += abs(rating1[key] - rating2[key]);
+            commonRatings = True;
+    if commonRatings:
+        return distance;
+    else:
+        return -1 #Indicates no ratings in common
---
+++ python-study/demo.py
@@ -0,0 +1,14 @@
+classmates = ['Michael', 'Bob', 'Tracy'];
+classmates.append('hejun')
+print(classmates);
+d = {'Michael': 95, 'Bob': 75, 'Tracy': 85};
+print(d['Michael']);
+if d.get('bbb') == None :
+    print('no fond');
+else :
+    print('fond');
+
+print(abs(-10));
+
+def power(x):
+    return x*x;
